3da2f5ac1835973fbb510f088953064d0c6c3a7f
TriHuynh <huynhminhtri1109@gmail.com>
2024-12-30 21:04:04 +0700

    a

---
+++ DoAnCuoiKi/frm/frm_BaoCao/BaoCaoDoanhThu.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace DoAnCuoiKi.frm.frm_BaoCao
+{
+    partial class BaoCaoDoanhThu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.lblTotalRevenue = new System.Windows.Forms.Label();
+            this.dgvReport = new System.Windows.Forms.DataGridView();
+            this.btnViewReport = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Location = new System.Drawing.Point(26, 58);
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(100, 22);
+            this.txtMonth.TabIndex = 9;
+            // 
+            // lblTotalRevenue
+            // 
+            this.lblTotalRevenue.AutoSize = true;
+            this.lblTotalRevenue.Location = new System.Drawing.Point(23, 466);
+            this.lblTotalRevenue.Name = "lblTotalRevenue";
+            this.lblTotalRevenue.Size = new System.Drawing.Size(113, 16);
+            this.lblTotalRevenue.TabIndex = 8;
+            this.lblTotalRevenue.Text = "Tổng doanh thu: 0";
+            // 
+            // dgvReport
+            // 
+            this.dgvReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReport.Location = new System.Drawing.Point(23, 96);
+            this.dgvReport.Name = "dgvReport";
+            this.dgvReport.RowHeadersWidth = 51;
+            this.dgvReport.RowTemplate.Height = 24;
+            this.dgvReport.Size = new System.Drawing.Size(572, 350);
+            this.dgvReport.TabIndex = 7;
+            // 
+            // btnViewReport
+            // 
+            this.btnViewReport.Location = new System.Drawing.Point(159, 58);
+            this.btnViewReport.Name = "btnViewReport";
+            this.btnViewReport.Size = new System.Drawing.Size(100, 23);
+            this.btnViewReport.TabIndex = 6;
+            this.btnViewReport.Text = "Xem báo cáo";
+            this.btnViewReport.UseVisualStyleBackColor = true;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(653, 96);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(471, 350);
+            this.chart1.TabIndex = 10;
+            this.chart1.Text = "chart1";
+            // 
+            // BaoCaoDoanhThu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1151, 502);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.lblTotalRevenue);
+            this.Controls.Add(this.dgvReport);
+            this.Controls.Add(this.btnViewReport);
+            this.Name = "BaoCaoDoanhThu";
+            this.Text = "BaoCaoDoanhThu";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReport)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.Label lblTotalRevenue;
+        private System.Windows.Forms.DataGridView dgvReport;
+        private System.Windows.Forms.Button btnViewReport;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
